ead18d52d66b576f733c903359d6a24b5f83f3d1
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-10-25 04:33:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
   name = "do_username",
   version = "1.0.0",
   packages = ["do_username"],
+  license = "MIT",
+  author = "Shreyas",
+  author_email = "shreyas.sreenivasa@gmail.com",
+  url = "https://github.com/shreyas44/do_username_py",
   entry_points= {
     "console_scripts": [
       "do_username = do_username.__main__:main"
